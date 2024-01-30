1d83161703cec545e9c76dbb7b6da46c33aec01b
lakshmanan1205 <lakshmananmass143@gmail.com>
2024-01-30 22:29:05 +0530

    implement script for webview

--- script.js
+++ script.js
@@ -1,64 +1,67 @@
+window.addEventListener("message", (message) => {
+  console.log("sri-laksh", message.data); // Wayne is coming!!!
+  window.ReactNativeWebView.postMessage("Client received data");
+});
 //VARIABLES
-const quoteContainerElement=document.getElementById("quote_container");
-const quoteTextElement=document.getElementById("quote");
-const authorTextElement=document.getElementById("author");
-const twitterBtnElement=document.getElementById("twitter");
-const newQuoteBtnElement=document.getElementById("new_quote");
-const loaderElement=document.getElementById("loader");
+const quoteContainerElement = document.getElementById("quote_container");
+const quoteTextElement = document.getElementById("quote");
+const authorTextElement = document.getElementById("author");
+const twitterBtnElement = document.getElementById("twitter");
+const newQuoteBtnElement = document.getElementById("new_quote");
+const loaderElement = document.getElementById("loader");
 
 let quotesList = [];
 // FUNCTIONS
 //FETCH QUOTES LIST FROM API
 async function getQuotes() {
-    loading();
-    const quoteUrl = "https://type.fit/api/quotes";
-    try {
-        const apiQuotesList = await fetch(quoteUrl);
-        quotesList = await apiQuotesList.json();
-        makeQuote()
-    } catch (error) {
-        console.log(error);
-    }
-
+  loading();
+  const quoteUrl = "https://type.fit/api/quotes";
+  try {
+    const apiQuotesList = await fetch(quoteUrl);
+    quotesList = await apiQuotesList.json();
+    makeQuote();
+  } catch (error) {
+    console.log(error);
+  }
 }
 //GET INDIVIDUAL QUOTE FROM QUOTES LIST
 function makeQuote() {
-    loading();
-    let quote = quotesList[Math.floor(Math.random() * quotesList.length)];
+  loading();
+  let quote = quotesList[Math.floor(Math.random() * quotesList.length)];
 
-    // check author if not available make it as "Unnown"
-    if (quote.author) {
-        authorTextElement.textContent=quote.author;
-    } else {
-        authorTextElement.textContent="Unknown"        
-    }
-    //change fontsize when quote text is length is long
-    if (quote.text.length>50) {
-        quoteTextElement.classList.add('long_quote');
-    } else {
-        quoteTextElement.classList.remove('long_quote');
-    }
-    quoteTextElement.textContent=quote.text;
-    complete()
+  // check author if not available make it as "Unnown"
+  if (quote.author) {
+    authorTextElement.textContent = quote.author;
+  } else {
+    authorTextElement.textContent = "Unknown";
+  }
+  //change fontsize when quote text is length is long
+  if (quote.text.length > 50) {
+    quoteTextElement.classList.add("long_quote");
+  } else {
+    quoteTextElement.classList.remove("long_quote");
+  }
+  quoteTextElement.textContent = quote.text;
+  complete();
 }
 //SET A TWEET WHEN CLICKED
-function makeTweet(){
-    const tweetUrl=`https://twitter.com/intent/tweet?text=${quoteTextElement.textContent} - ${authorTextElement.textContent}`;
-    window.open(tweetUrl,'_blank')
+function makeTweet() {
+  const tweetUrl = `https://twitter.com/intent/tweet?text=${quoteTextElement.textContent} - ${authorTextElement.textContent}`;
+  window.open(tweetUrl, "_blank");
 }
 //LOADER SHOW
-function loading(){
-    loaderElement.hidden=false;
-    quoteContainerElement.hidden=true;
+function loading() {
+  loaderElement.hidden = false;
+  quoteContainerElement.hidden = true;
 }
-function complete(){
-    quoteContainerElement.hidden=false;
-    loaderElement.hidden=true;
+function complete() {
+  quoteContainerElement.hidden = false;
+  loaderElement.hidden = true;
 }
 //  EVENT LISTENERS
-newQuoteBtnElement.addEventListener('click',makeQuote);
-twitterBtnElement.addEventListener('click',makeTweet);
+newQuoteBtnElement.addEventListener("click", makeQuote);
+twitterBtnElement.addEventListener("click", makeTweet);
 
 //ON LOAD
 // loading()
-getQuotes();//generate quotes from online using api
+getQuotes(); //generate quotes from online using api
